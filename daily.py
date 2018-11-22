021d3a3af46e2d90197561c203443617e2b2546a
Marley Ferreira-Malyon <kojimajr@users.noreply.github.com>
2018-11-22 16:27:20 -0500

    broken daily.py

--- daily.py
+++ daily.py
@@ -1,11 +1,31 @@
 import sys
+import subprocess
 
 day = sys.argv[1]
-
-
 
 for i in range(1,4):
     commands = open('io/commands/day_'+str(day)+"/"+str(i)+".txt")
     front_process = subprocess.Popen(['python', 'front/front.py', 'io/vsl/'+ day + ".txt", 'io/tsf/day_'+day+'/'+str(i)+'.txt'], stdin=commands)
     front_process.wait()
 
+mtsf = open('io/mtsf/'+ day +'.txt', "w")
+
+for i in range(1,4):
+    tsf = open('io/tsf/day_'+day+'/'+str(i)+'.txt', "r")
+    for line in tsf:
+        if not "EOS" in line:
+            mtsf.write(line)
+
+mtsf.write("EOS 00000 0 00000 **** 0")
+
+back_process = subprocess.Popen(
+    [
+        'python', 'back/back.py', 
+        'io/csf/'+ day + ".txt", 
+        'io/mtsf/'+ day + ".txt", 
+        'io/csf/'+ str(int(day) + 1) + ".txt", 
+        'io/vsl/'+ str(int(day) + 1) + ".txt", 
+    ]
+)
+
+back_process.wait()
