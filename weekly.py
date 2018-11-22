e390f789af1a1caac0d34892947f2c0837e27395
James Ford <jackamo96@gmail.com>
2018-11-22 16:46:57 -0500

    fixed weekly.py bug

--- weekly.py
+++ weekly.py
@@ -2,5 +2,5 @@ import sys
 import subprocess
 
 for i in range(1,6):
-    daily_process = subprocess.Popen(['python3', 'dayTest.py', str(i)])
+    daily_process = subprocess.Popen(['python3', 'daily.py', str(i)])
     daily_process.wait()
