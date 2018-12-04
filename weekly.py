a5a0df178284fe4dfc5ef0160de5817109fc92f4
Marley Ferreira-Malyon <kojimajr@users.noreply.github.com>
2018-12-03 20:37:25 -0500

    Finished

--- weekly.py
+++ weekly.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 
-# run the daily script 5 times
+# run the daily script 5 times, on 5 seperate "days" of input
 for i in range(1,6):
     daily_process = subprocess.Popen(['python3', 'daily.py', str(i)])
     daily_process.wait()
