9ae0e9d293c3fecd919e2a709fb5b9ef2c353b25
scahalan <13sc77@queensu.ca>
2018-12-03 20:30:49 -0500

    add comments to daily and weekly

--- weekly.py
+++ weekly.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
 
+# run the daily script 5 times
 for i in range(1,6):
     daily_process = subprocess.Popen(['python3', 'daily.py', str(i)])
     daily_process.wait()
